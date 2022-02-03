bc6021278bfa9a785511bf892187fa36b27f3387
Marina <msh261288@gmail.com>
2022-02-03 20:35:39 +0300

    change test names

--- Searching/binarySearch/binarySearch.spec.js
+++ Searching/binarySearch/binarySearch.spec.js
@@ -5,25 +5,25 @@ const { binarySearch } = require("./binarySearch");
 
 describe("binary search", () => {
 
-    it("number has found in array", () => {
+    it("the number is in the array", () => {
         const array = [1, 2, 3, 4, 5, 6, 7, 8];
         const actual = binarySearch(array, 8);
         assert.strictEqual(actual, 7);
     });
 
-    it("number hasn't found in array", () => {
+    it("the number isn't in the array", () => {
         const array = [1, 2, 3, 4, 5, 6, 7];
         const actual = binarySearch(array, 8);
         assert.strictEqual(actual, -1);
     });
 
-    it("array with the one same element", () => {
+    it("the number is in the one-element array", () => {
         const array = [42];
         const actual = binarySearch(array, 42);
         assert.strictEqual(actual, 0);
     });
 
-    it("array with one different element", () => {
+    it("the number isn't in the one-element array", () => {
         const array = [99];
         const actual = binarySearch(array, 42);
         assert.strictEqual(actual, -1);
